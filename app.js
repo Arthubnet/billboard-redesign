39a48f8bef2765ce00a548ce5d4ba6f235cf285e
Lena <z.otig@yahoo.com>
2022-06-27 17:31:54 -0600

    arrow scroll up

--- app.js
+++ app.js
@@ -277,3 +277,39 @@ moreNews.innerHTML = moreNewsData
           </div>`;
   })
   .join("");
+
+/* Scroll up arrow */
+
+const offset = 400;
+const scrollUp = document.querySelector(".scroll-up");
+const scrollUpSvgPath = document.querySelector(".scroll-up__svg-path");
+const pathLength = scrollUpSvgPath.getTotalLength();
+
+scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+scrollUpSvgPath.style.transition = "stroke-dashoffset 20ms";
+
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+//update Dashoffset
+
+const updateDashoffset = () => {
+  const height = document.documentElement.scrollHeight - window.innerHeight;
+  const dashoffset = pathLength - (getTop() * pathLength) / height;
+  scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+};
+// onScroll
+window.addEventListener("scroll", () => {
+  updateDashoffset();
+  if (getTop() > offset) {
+    scrollUp.classList.add("active");
+  } else {
+    scrollUp.classList.remove("active");
+  }
+});
+// click
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
